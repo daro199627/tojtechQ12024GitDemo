837552a7a3845ebe580b5810766b11959cbd2d75
Darina <eradze.de@gmail.com>
2024-06-25 19:40:35 -0400

    Indian timezone new changes

--- tojtechQ12024frameworkFailure/tests/conftest.py
+++ tojtechQ12024frameworkFailure/tests/conftest.py
@@ -9,9 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def setup(request):
     options = Options()
     options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+    options.add_argument("start-maximized")
+    service_obj = Service("/opt/homebrew/bin/chromedriver")
+    driver = webdriver.Chrome(service=service_obj, options=options)
+    driver.implicitly_wait(15)
     driver.get("http://staging.shopping.beeyor.com/shop/")
     request.cls.driver = driver
     yield
